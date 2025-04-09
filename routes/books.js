82188dcf06bb3bdab28051e090e901e408e24a30
Saurabh-Arya16 <149648080+Saurabh-Arya16@users.noreply.github.com>
2025-04-09 17:57:32 +0530

    books.js

--- routes/books.js
+++ routes/books.js
@@ -17,13 +17,15 @@ router.get('/seed-books', async (req, res) => {
     await Book.deleteMany({});
     await Book.insertMany([
       {
-        title: "Learn JavaScript",
-        author: "John Doe",
-        description: "A complete guide to mastering JS.",
+        image:"harry.jpg",
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        description: "A magical journey of Harry Potter.",
         price: 399,
         level: "beginner"
       },
       {
+        image:"mongo.png",
         title: "MongoDB in Action",
         author: "Jane Smith",
         description: "Understand MongoDB with real-world examples.",
@@ -31,8 +33,81 @@ router.get('/seed-books', async (req, res) => {
         level: "intermediate"
       },
       {
+        image:"node.png",
         title: "Node.js Mastery",
         author: "Dev Expert",
+        description: "Become a backend expert with Node.js.",
+        price: 599,
+        level: "advanced"
+      },
+      {
+        image:"lord.jpg",
+        title: "Lord of rings",
+        author: "J.R.R. Tolkien",
+        description: "A magical journey of Harry Potter.",
+        price: 399,
+        level: "beginner"
+      },
+      {
+        image:"math.jpg",
+        title: "Maths",
+        author: "R.D. Sharma",
+        description: "Understand MongoDB with real-world examples.",
+        price: 499,
+        level: "intermediate"
+      },
+      {
+        image:"invisible.jpg",
+        title: "invisible man",
+        author: "H.G. Wells",
+        description: "Become a backend expert with Node.js.",
+        price: 599,
+        level: "advanced"
+      },
+      {
+        image:"cs.jpg",
+        title: "Computer Science",
+        author: "J.K. Rowling",
+        description: "A magical journey of Harry Potter.",
+        price: 399,
+        level: "beginner"
+      },
+      {
+        image:"english.jpg",
+        title: "English",
+        author: "Jane Smith",
+        description: "Understand MongoDB with real-world examples.",
+        price: 499,
+        level: "intermediate"
+      },
+      {
+        image:"php.jpg",
+        title: "PHP",
+        author: "Dev Expert",
+        description: "Become a backend expert with Node.js.",
+        price: 599,
+        level: "advanced"
+      },
+      {
+        image:"science.jpg",
+        title: "Lord of rings",
+        author: "J.R.R. Tolkien",
+        description: "A magical journey of Harry Potter.",
+        price: 399,
+        level: "beginner"
+      },
+      {
+        image:"sql.jpg",
+        title: "Maths",
+        author: "R.D. Sharma",
+        description: "Understand MongoDB with real-world examples.",
+        price: 499,
+        level: "intermediate"
+      },
+      {
+        image:"web.jpg",
+        title: "invisible man",
+        author: "H.G. Wells",
         description: "Become a backend expert with Node.js.",
         price: 599,
         level: "advanced"
